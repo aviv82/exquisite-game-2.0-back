d307bb2799cb59c63cc24b461f22c2e1ebb08cec
Aviv <aviv.dror@gmail.com>
2022-07-08 22:34:20 +0200

    delete faux user type

--- src/api/exquisite/controllers/exquisite.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  exquisite controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::exquisite.exquisite');
--- src/api/exquisite/services/exquisite.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * exquisite service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::exquisite.exquisite');
